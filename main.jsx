bcb67878809a05b7adeeea5aefcca712466e9211
Mayssa-briki <beenflong@gmail.com>
2026-01-04 05:05:25 -1100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+import LoveQuiz from "./quiz.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LoveQuiz />
+  </React.StrictMode>
+);
